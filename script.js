5033e9b69f248a39a33acd155aca173c2cf02d0f
mario <126523180+usermario4@users.noreply.github.com>
2023-06-15 13:30:37 +0300

    movingcircle

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function startGame() {
   countdown = 0;
   timer = setInterval(updateCountdown, 1000);
 }
+
 function updateCountdown() {
   countdown++;
   document.getElementById("countdown").innerHTML = countdown.toString();
@@ -21,40 +22,39 @@ function updateCountdown() {
 }
 
 class TheSquare {
-  #x;
-  #y;
-  #color;
   constructor(x, y, color) {
-    this.#color = color;
-    this.#x = x;
-    this.#y = y;
+    this.x = x;
+    this.y = y;
+    this.color = color;
   }
+
   draw() {
-    fill(this.#color);
-    rect(this.#x, this.#y, 60, 50, 5);
+    fill(this.color);
+    rect(this.x, this.y, 60, 50, 5);
   }
+
   setColor(color) {
-    this.#color = color;
+    this.color = color;
   }
 }
 
-class theFrame {
-  #x;
-  #y;
-  #color;
+class TheFrame {
   constructor(x, y, color) {
-    this.#color = color;
-    this.#x = x;
-    this.#y = y;
+    this.x = x;
+    this.y = y;
+    this.color = color;
   }
+
   draw() {
-    fill(this.#color);
-    rect(this.#x, this.#y, 90, 45, 10);
+    fill(this.color);
+    rect(this.x, this.y, 90, 45, 10);
   }
+
   setColor(color) {
-    this.#color = color;
+    this.color = color;
   }
 }
+
 
 function createMatrix(Rows, numCols, squareSize, startX, startY, color, distance) {
   const matrix = [];
@@ -64,10 +64,6 @@ function createMatrix(Rows, numCols, squareSize, startX, startY, color, distance
       const x = startX + j * squareSize + j * distance;
       const y = startY + i * squareSize - i * distance;
       const square = new TheSquare(x, y, color);
-      //if (i < Rows && (j === 0 || j === numCols - 1)) {
-      //    square.setColor("#FF0000");
-      //   }
-
       row.push(square);
     }
     matrix.push(row);
@@ -76,36 +72,29 @@ function createMatrix(Rows, numCols, squareSize, startX, startY, color, distance
 }
 
 function createFrame(rows, numCols, frameSize, startX, startY, color, distance) {
-  const matrix1 = [];
+  const matrix = [];
   for (let i = 0; i < rows; i++) {
     const row = [];
     for (let j = 0; j < numCols; j++) {
       const x = startX + j * frameSize - j * distance;
       const y = startY + i * frameSize + i * distance;
-
-
-      const frame = new theFrame(x, y, color);
+      const frame = new TheFrame(x, y, color);
       row.push(frame);
     }
-    matrix1.push(row);
+    matrix.push(row);
   }
-  return matrix1;
+  return matrix;
 }
-function createFrame(rows, numCols, frameSize, startX, startY, color, distance) {
-  const matrix2 = [];
-  for (let i = 0; i < rows; i++) {
-    const row = [];
-    for (let j = 0; j < numCols; j++) {
-      const x = startX + j * frameSize - j * distance;
-      const y = startY + i * frameSize + i * distance;
-
-
-      const frame = new theFrame(x, y, color);
-      row.push(frame);
-    }
-    matrix2.push(row);
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    circleX -= 70; // Move the circle to the left by 10 units
+  } else if (keyCode === RIGHT_ARROW) {
+    circleX += 70; // Move the circle to the right by 10 units
+  } else if (keyCode === UP_ARROW) {
+    circleY -= 60; // Move the circle up by 10 units
+  } else if (keyCode === DOWN_ARROW) {
+    circleY += 60; // Move the circle down by 10 units
   }
-  return matrix2;
 }
 
 let matrix = [];
@@ -124,7 +113,7 @@ function setup() {
 }
 
 function draw() {
-  background("#222222");
+  background("#2B1B17");
   for (let i = 0; i < matrix.length; i++) {
     const row = matrix[i];
     for (let j = 0; j < row.length; j++) {
@@ -146,5 +135,5 @@ function draw() {
       frame.draw();
     }
   }
-  ;
+  drawCircle(circleX, circleY); 
 }
