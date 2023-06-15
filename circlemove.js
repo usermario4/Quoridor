e0b87ca0396681d2dc6bc8cf45dc51c5afab54bd
mario <126523180+usermario4@users.noreply.github.com>
2023-06-15 12:52:28 +0300

    circle folder

---
+++ circlemove.js
@@ -0,0 +1,9 @@
+
+  
+  function drawCircle(x, y) {
+    fill(255, 0, 0); // Set fill color to red
+    ellipse(x, y, 50, 50); // Draw a circle at the specified position
+  }
+    drawCircle(200, 200);
+  
+  
