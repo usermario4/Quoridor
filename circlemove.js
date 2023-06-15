5186dd539fe7b9f5d4f6b3aeb34ff4191a10c230
mario <126523180+usermario4@users.noreply.github.com>
2023-06-15 14:26:36 +0300

    moving circle 2

--- circlemove.js
+++ circlemove.js
@@ -1,9 +1,36 @@
 
-  
-  function drawCircle(x, y) {
-    fill(255, 0, 0); // Set fill color to red
-    ellipse(x, y, 50, 50); // Draw a circle at the specified position
+let circle1X = 560;
+let circle1Y = 150;
+
+let circle2X=560;
+let circle2Y=630;
+const boundryLeft=279;
+
+  function drawCircle(x, y,color) {
+    fill(color);
+    ellipse(x, y, 40, 40); // Draw a circle at the specified position
   }
-    drawCircle(200, 200);
-  
+  function keyPressed() {if(circle1X>279){
+    if (keyCode === LEFT_ARROW ) {
+     if (circle1X>boundryLeft)
+     { circle1X -= 70;}
+    } else if (keyCode === RIGHT_ARROW) {
+      circle1X += 70;
+    } else if (keyCode === UP_ARROW) {
+      circle1Y -= 60;
+    } else if (keyCode === DOWN_ARROW) {
+      circle1Y += 60;
+    }
+    if (key === 'a' || key === 'A') {
+      circle2X -= 70;
+    } else if (key === 'd' || key === 'D') {
+      circle2X += 70;
+    } else if (key === 'w' || key === 'W') {
+      circle2Y -= 60;
+    } else if (key === 's' || key === 'S') {
+      circle2Y += 60;
+    }
+  }
+  }
+ 
   
